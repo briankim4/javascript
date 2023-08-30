3a49197cb32e48f696bea893850c34c551ffbee4
briankim4 <briankimaiyo@gmail.com>
2023-08-30 22:35:18 +0300

    first commit

---
+++ java.java
@@ -0,0 +1,11 @@
+
+  public class main  {
+
+  piblic static void main(string[] args) {
+       
+         system.out.printin("i love pizza")
+         system.out.printin( "its reaaly good")
+       
+       )
+
+)
